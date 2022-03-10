fc50b7b675f0f7237d5a0df6307246dc72e6c14d
Pablo Cunha <p.abloc1@hotmail.com>
2022-03-09 21:57:12 -0300

    Footer Pronto

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8c6b75940089412"; }
+/******/ 		__webpack_require__.h = function() { return "98602c58b0a90db8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,27 @@
 import react from "react";
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function Footer() {
     return (
         <div className="footer-container">
             <nav className="footer-nav">
-            <a className="hyper-text" href="">Github • </a>
-            <a className="hyper-text" href="">Linkedin • </a>
-            <a className="hyper-text" href="">Portfolio</a>
+            <a className="hyper-text" 
+            href="https://github.com/pablo-cunha" 
+            target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="2x"/>
+            </a>
+            <a className="hyper-text" 
+            href="https://www.linkedin.com/in/pablo-cunha/"
+            target="_blank">
+            <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
+            </a>
+            <a className="hyper-text" 
+            href="https://www.pablocunha.com/"
+            target="_blank">
+            <FontAwesomeIcon icon={faUserAstronaut} size="2x"/>
+            </a>
             </nav>
         </div>
     )
