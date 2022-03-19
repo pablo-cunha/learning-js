b5579e9dbc33989194b8b0d2697bab74ea0054b7
Pablo Cunha <p.abloc1@hotmail.com>
2022-03-18 21:27:51 -0300

    Syntax Highlighter aplicado

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,19 +1,29 @@
 import react from "react";
-import parse from 'html-react-parser'
 import Header from "./Header";
 import Bio from "./Bio"
 import Footer from "./Footer";
 import ReactMarkdown from 'react-markdown'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { gradientDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
-function Post(props) {
+
+export default function Post(props) {
     return (
         <div className="post-container">
             <Header>Learning JS</Header>
-            <ReactMarkdown children={props.posts[props.index].content}/>
+            <ReactMarkdown components={{ code: ({ node, ...props }) => <SyntaxComponent {...props} /> }}>
+                {props.posts[props.index].content}
+            </ReactMarkdown>
             <Bio/>
             <Footer/>
         </div>
     )
 }
 
-export default Post
+const SyntaxComponent = (props) => {
+    return (
+        <SyntaxHighlighter language={props.language} style={gradientDark}>
+          {props.children}
+        </SyntaxHighlighter>
+      )
+}
